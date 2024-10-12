de65a96c7eb6193b6fedab5f04c79f41311a57df
neddamj <jordanmadden285@gmail.com>
2024-10-12 10:13:55 -0400

    interface to attack both neural and algorithmic compression

--- compressor.py
+++ compressor.py
@@ -1,12 +1,48 @@
-class Compressor:
-    """
-        I need this to be an interface through which I can input a neural compression model 
-        or a classical compression model (JPEG etc) and have it spit out the relevant outputs
-        so that I can calculate the gradients of the inputs w.r.t the loss function.
+import torch 
+from load_compression_models import (
+    my_bmshj2018_factorized, 
+    my_bmshj2018_factorized_relu,
+    my_bmshj2018_hyperprior,
+    my_cheng2020_anchor,
+    my_cheng2020_attn,
+    my_mbt2018,
+    my_mbt2018_mean
+)
 
-    """
+class NeuralCompressor:
+    def __init__(self, model_name, quality_factor=2, device='cpu'):
+        models = {
+            'my_bmshj2018_factorized_relu': my_bmshj2018_factorized_relu(quality=quality_factor, pretrained=True).train().to(torch.float32).to(device),
+            'my_bmshj2018_factorized'     : my_bmshj2018_factorized(quality=quality_factor, pretrained=True).train().to(torch.float32).to(device),
+            'my_bmshj2018_hyperprior'     : my_bmshj2018_hyperprior(quality=quality_factor, pretrained=True).train().to(torch.float32).to(device),
+            'my_cheng2020_anchor'         : my_cheng2020_anchor(quality=quality_factor, pretrained=True).train().to(torch.float32).to(device),
+            'my_cheng2020_attn'           : my_cheng2020_attn(quality=quality_factor, pretrained=True).train().to(torch.float32).to(device),
+            'my_mbt2018'                  : my_mbt2018(quality=quality_factor, pretrained=True).train().to(torch.float32).to(device),
+            'my_mbt2018_mean'             : my_mbt2018_mean(quality=quality_factor, pretrained=True).to(torch.float32).train().to(device)
+        }
+        self.model = models[model_name]
+
+    def __call__(self, x):
+        return self.model(x)['y_hat']
+    
+    def run(self, x):
+        return self.model(x)
+
+    def compress(self, x):
+        return self.model.compress(x)['strings']
+
+    def decompress(self, x_hat):
+        return self.model.decompress(x_hat)
+
+class JpegCompressor:
     def __init__(self):
         pass
 
-    def __call__(self, *args, **kwds):
+    def __call__(self, x):
+        pass
+
+    def compress(self):
+        pass
+
+    def decompress(self):
         pass
